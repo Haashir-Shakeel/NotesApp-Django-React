28be6ebc93c0b48a0caf6f58b981ae447dd13455
Haashir-Shakeel <david10james99@gmail.com>
2022-08-30 14:11:17 +0500

    making api restful

--- api/urls.py
+++ api/urls.py
@@ -5,17 +5,18 @@ urlpatterns=[
     path('', views.getRoutes, name='routes'),
     path('notes/',views.getNotes, name='notes'),
 
-    path('notes/create/', views.createNote, name='create-note'),
-    #there was error when we place this url below the getNote url
+    # path('notes/create/', views.createNote, name='create-note'),
+
+    #there was error when we place above url below the getNote url
     #error: Method POST not allowed
 
     path('notes/<str:pk>/', views.getNote, name='note'),
 
     
 
-    path('notes/<str:pk>/update/', views.updateNote, name='update-note'),
+    # path('notes/<str:pk>/update/', views.updateNote, name='update-note'),
 
-    path('notes/<str:pk>/delete/', views.deleteNote, name='delete-note'),
+    # path('notes/<str:pk>/delete/', views.deleteNote, name='delete-note'),
 
     
     
--- api/views.py
+++ api/views.py
@@ -11,48 +11,82 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+# /notes GET
+# /notes POST
+# /notes/<id> GET
+# /notes/<id> PUT
+# /notes/<id> DELETE
+
 @api_view(['GET'])
 def getRoutes(request):
     return Response('Our API')
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getNotes(request):
-    notes = Note.objects.all().order_by('-updated')
-    serializer = NoteSerializer(notes,many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        notes = Note.objects.all().order_by('-updated')
+        serializer = NoteSerializer(notes,many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        data = request.data
+        note = Note.objects.create(
+            body=data['body']
+        )
+        serializer = NoteSerializer(note, many=False)
+        return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def getNote(request, pk):
-    print(request.data)
-    notes = Note.objects.get(id=pk)
-    serializer = NoteSerializer(notes,many=False)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        notes = Note.objects.get(id=pk)
+        serializer = NoteSerializer(notes,many=False)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        data = request.data # can use data because we using rest_framework otherwise we do request.body or request.POST
+        note = Note.objects.get(id=pk)
+        serialzer = NoteSerializer(note, data = data)
+
+        if serialzer.is_valid():
+            serialzer.save()
+            return Response(serialzer.data)
+        return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        note = Note.objects.get(id=pk)
+        note.delete()
+        return Response('Note was deleted')
+
+        
 
 
-@api_view(['POST'])
-def createNote(request):
-    data = request.data
-    note = Note.objects.create(
-        body=data['body']
-    )
-    serializer = NoteSerializer(note, many=False)
-    return Response(serializer.data)
 
-@api_view(['PUT'])
-def updateNote(request, pk):
-    data = request.data # can use data because we using rest_framework otherwise we do request.body or request.POST
-    note = Note.objects.get(id=pk)
-    serialzer = NoteSerializer(note, data = data)
 
-    if serialzer.is_valid():
-        serialzer.save()
-        return Response(serialzer.data)
-    return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# def createNote(request):
+#     data = request.data
+#     note = Note.objects.create(
+#         body=data['body']
+#     )
+#     serializer = NoteSerializer(note, many=False)
+#     return Response(serializer.data)
 
-@api_view(['DELETE'])
-def deleteNote(request, pk):
-    note = Note.objects.get(id=pk)
-    note.delete()
-    return Response('Note was deleted')
+# @api_view(['PUT'])
+# def updateNote(request, pk):
+#     data = request.data # can use data because we using rest_framework otherwise we do request.body or request.POST
+#     note = Note.objects.get(id=pk)
+#     serialzer = NoteSerializer(note, data = data)
+
+#     if serialzer.is_valid():
+#         serialzer.save()
+#         return Response(serialzer.data)
+#     return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['DELETE'])
+# def deleteNote(request, pk):
+#     note = Note.objects.get(id=pk)
+#     note.delete()
+#     return Response('Note was deleted')
